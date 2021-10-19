4e796662d5f7d5a81008100786a24b0bc1131c60
syt06162 <syt06162@naver.com>
2021-10-19 22:09:24 +0900

    17298 commit (2)

--- baekjoon/17298.py
+++ baekjoon/17298.py
@@ -2,20 +2,15 @@ import sys
 
 N = int(input())
 numList = list(map(int, sys.stdin.readline().split()))
-ngeList = [-1 for i in range(len(numList))]
-stack = []  #<value, index>
+ngeList = [-1 for _ in range(len(numList))]
+stack = []  #<index>
 
-index = 0
-stack.append(index)
-index += 1
-
-while index<len(numList):
+for index in range(N):
     value = numList[index]
     while stack and numList[stack[-1]] < value:
         st_index = stack.pop()
         ngeList[st_index] = value
     stack.append(index)
-    index += 1
 
 for i in ngeList:
     print(i, end=" ")

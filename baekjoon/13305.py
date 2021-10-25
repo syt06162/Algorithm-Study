105c2943ae4a7b78ec7338626d6a626904ca7f5e
syt06162 <syt06162@naver.com>
2021-10-25 11:25:08 +0900

    13305 commit (2)

--- baekjoon/13305.py
+++ baekjoon/13305.py
@@ -5,15 +5,16 @@ N = int(input())
 dis = list(map(int, sys.stdin.readline().split())) # [다음 도시까지의 거리]
 price = list(map(int, sys.stdin.readline().split())) # [리터당 가격]
 
-dis.append(0)
-price.append(0)
+dis.append(0) #종료를 위해 마지막에 0을 붙임
+price.append(0) #종료를 위해 마지막에 0을 붙임
 
 currentIdx = 0
 nextIdx = 1
 
 result = 0
 while currentIdx != len(price)-1:
-    while True:
+    # 현위치 가격보다 더 싼 가격이 있는 곳까지 거리만 기름을 넣는다.
+    while True: 
         if price[nextIdx] >= price[currentIdx]:
             nextIdx += 1
         else:
@@ -23,9 +24,3 @@ while currentIdx != len(price)-1:
     nextIdx += 1
 
 print(result)
-
-
-
-# 10
-# 1 2 3 4 3 2 1 2 3
-# 5 4 2 3 4 1 6 4 2 3

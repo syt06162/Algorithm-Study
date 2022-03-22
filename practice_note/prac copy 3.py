d97965429f5f68eca8102b3ff8885520d2010199
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-03-22 19:08:43 +0900

    debug practice python, vs code

--- practice_note/prac copy 3.py
+++ practice_note/prac copy 3.py
@@ -1,3 +1,15 @@
-from itertools import combinations
+# debug 연습
 
-print(list(combinations([1,2,3],2)))
+a = 23
+
+def find_dog(sound):
+    if sound == "멍멍":
+        return("개가 짖네")
+
+    else:
+        return("다른 동물이구나")
+
+sound = "야옹"
+find_result = find_dog(sound)
+
+print(find_result)

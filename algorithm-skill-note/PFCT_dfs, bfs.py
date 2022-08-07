7d540685a2247ae2f755814492f0d1c984313e51
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-08-08 02:08:19 +0900

    fix: PFCT dfs bfs

--- algorithm-skill-note/PFCT_dfs, bfs.py
+++ algorithm-skill-note/PFCT_dfs, bfs.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 # 가능하면 visited를 꼭 쓰자! 안그러면 무한루프에 빠지거나, 엄청난 시간초과가 뜬다!
+# 둘다 쓸 수 있는 상황이면 보통 bfs가 더 빠른 것 같다.
 
+# dfs 는 심플하게 재귀
 def dfs(graph: list, v:int, visited: list):
     visited[v] = True
     print(v, end=" ")
     for i in graph[v]:
         if visited[i] == False:
             dfs(graph, i, visited)
-
-
 
 # bfs에서 중요한 점은, visited=True 를 어디에 처리할지이다.
 # Q에서 pop 하고나서 처리할 경우 여러개의 같은 것이 중복으로 Q에 들어가게 될 수도 있으니,

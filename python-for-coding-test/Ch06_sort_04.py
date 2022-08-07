b5a5f72d609de137cf79119e5cd3bd084089b863
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-08-08 02:27:52 +0900

    fix: PFCT 06-04 fix

--- python-for-coding-test/Ch06_sort_04.py
+++ python-for-coding-test/Ch06_sort_04.py
@@ -7,9 +7,18 @@ a = list(map(int, sys.stdin.readline().split()))
 b = list(map(int, sys.stdin.readline().split()))
 
 a.sort()
-b.sort()
+b.sort(reverse=True)
 
-sum2 = 0
-sum2 += sum(a[K:])
-sum2 += sum(b[-K:])
-print(sum2)
+#### 잘못된 풀이, b 배열에서의 선택 요소가 더 작은 경우는 바꾸지 않아야 함.
+# sum2 = 0
+# sum2 += sum(a[K:])
+# sum2 += sum(b[-K:])
+# print(sum2)
+
+for i in range(K):
+    if b[i] > a[i]:
+        b[i] , a[i] = a[i], b[i]
+    else:
+        break
+
+print(sum(a))

a3bca36f83dbb72748a92170e13e49896a00b566
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-08-23 13:34:04 +0900

    fix: PFCT ch12_02 0 or 1 => plus

--- python-for-coding-test/Ch11_02_greedy.py
+++ python-for-coding-test/Ch11_02_greedy.py
@@ -1,24 +1,21 @@
 # Ch11_02
-# 핵심: 0이 계산식에 없으면 무조건 곱한다. 있으면 더한다.
+# 핵심: 0이나 1이 계산식에 없으면 무조건 곱한다. 있으면 더한다.
 
 import sys
 
 st = input()
 
 result = 0
-tempSum = 0
 for i in range(len(st)):
-    if tempSum == 0:
-        tempSum = int(st[i])
+    if result == 0:
+        result = int(st[i])
 
-    # 0이 아니면 곱하고
-    elif st[i] != '0':
-        tempSum *= int(st[i])
+    # 0이나 1이 아니면 곱하고
+    elif st[i] not in ['0', '1']:
+        result *= int(st[i])
 
-    # 0이 있으면 더한다.
+    # 0이나 1이 있으면 더한다.
     else:
-        result += tempSum
-        tempSum = 0
-result += tempSum
+        result += int(st[i])
 
 print(result)

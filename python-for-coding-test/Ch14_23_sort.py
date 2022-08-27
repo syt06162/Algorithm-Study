463d1ddc8fe839dc6ff053d110969a5cc2fe8cad
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-08-28 01:01:17 +0900

    PFCT ch14_23 sort

---
+++ python-for-coding-test/Ch14_23_sort.py
@@ -0,0 +1,16 @@
+# Ch13_22
+# boj 10825
+# 핵심: sort() 를 한번만 사용하므로, 감소순인 것들은 수 앞에 - 붙이기
+
+
+import sys
+
+N = int(input())
+students = []
+for i in range(N):
+    name, a,b,c = sys.stdin.readline().split()
+    students.append((name, -int(a), int(b), -int(c)))
+
+students.sort(key=lambda x: (x[1], x[2], x[3], x[0]))
+for i in range(N):
+    print(students[i][0])

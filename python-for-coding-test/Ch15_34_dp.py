a608814722b0ba0ab5a24d7e05461c76607d638c
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-09-04 23:29:13 +0900

    PFCT ch15_34 dp - LIS

---
+++ python-for-coding-test/Ch15_34_dp.py
@@ -0,0 +1,17 @@
+# Ch15_34
+# boj 18353
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, input().split()))
+nums.reverse()
+
+dp = [1 for i in range(N)]
+for i in range(N):
+    for j in range(i):
+        if nums[j] < nums[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(N - max(dp))

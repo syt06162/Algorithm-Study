0bcca92f77bb803970cbe6802beaaa35fb1238b6
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-09-05 03:00:38 +0900

    PFCT ch15_35 dp

---
+++ python-for-coding-test/Ch15_35_dp.py
@@ -0,0 +1,17 @@
+# Ch15_35
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+mot = [False, True] #0:false, 1:true
+i = 2
+while N>1:
+    if (i%2==0 and mot[i//2]==True) or (i%3==0 and mot[i//3]==True) or (i%5==0 and mot[i//5]==True):
+        mot.append(True)
+        N -= 1
+    else:
+        mot.append(False)
+    i += 1
+
+print(len(mot)-1)

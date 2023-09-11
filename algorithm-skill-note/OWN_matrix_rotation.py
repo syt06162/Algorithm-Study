9bff2ee68e9dab5a70d87f539470e24caba92fa1
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-09-12 00:03:45 +0900

    own: matrix rotation

--- algorithm-skill-note/OWN_matrix_rotation.py
+++ algorithm-skill-note/OWN_matrix_rotation.py
@@ -9,3 +9,13 @@ def rotate_90(old):
             new[j][N-1-i] = old[i][j]
     
     return new
+
+# 90, 180, 270:
+# https://shoark7.github.io/programming/algorithm/rotate-2d-array
+
+# with zip (90)
+import numpy as np
+
+def zip_rotate(original):
+    rotated = np.array(list(zip(*original[::-1])))
+    return rotated

298fab3fb417ce987501ae9d266f20519905fc3d
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-09-12 01:23:38 +0900

    PFCT: 0: fast input sys, other alters

---
+++ algorithm-skill-note/PFCT_0. fast input sys ★.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = []
+for i in range(N):
+    board.append(list(map(int, input().split())))  # 띄어쓰기 구분
+    board.append(list(map(int, input().strip())))  # 띄어쓰기 없이

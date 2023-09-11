cce8ce5cf71722a7877aecd08de244b7da6b26f7
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-09-12 00:06:24 +0900

    own: bisect library

--- algorithm-skill-note/PFCT_binary_search.py
+++ algorithm-skill-note/PFCT_binary_search.py
@@ -10,3 +10,10 @@ def binary_search(arr, target, start, end):
             end = mid-1
     return None
 
+from bisect import bisect, bisect_left, bisect_right
+
+lst = [1,2,5,5,7, 7,7,9,12,30]
+
+print(bisect(lst, 7)) # 7
+print(bisect_left(lst, 7)) # 4
+print(bisect_right(lst, 7)) # 7

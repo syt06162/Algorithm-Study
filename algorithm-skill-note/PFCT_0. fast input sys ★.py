b7cd2c33ad50718b90f3bafcdb439e31b38a1b25
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2023-09-15 14:44:33 +0900

    PFCT fast input

--- algorithm-skill-note/PFCT_0. fast input sys ★.py
+++ algorithm-skill-note/PFCT_0. fast input sys ★.py
@@ -1,10 +1,15 @@
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+
 N, M = map(int, input().split())
+
+
 board = []
 for i in range(N):
     board.append(list(map(int, input().split())))  # 띄어쓰기 구분
     board.append(list(map(int, input().strip())))  # 띄어쓰기 없이
+
 
 lst = list(map(int, input().split()))
